735414eb509a4a3a6fb8810e585947d2cdcea5fc
danny7117 <dtai2016@gmail.com>
2025-08-01 19:37:18 +0800

    Create update_catalog.js

---
+++ scripts/update_catalog.js
@@ -0,0 +1,15 @@
+// scripts/update_catalog.js
+import fs from 'fs';
+import glob from 'glob';
+
+const catalog = glob.sync('modules/**/manifest.json').map((file) => {
+  const m = JSON.parse(fs.readFileSync(file, 'utf8'));
+  return {
+    name: m.name,
+    path: file.replace('/manifest.json', ''),
+    tags: m.tags,
+    env: m.env,
+  };
+});
+fs.writeFileSync('module_catalog.json', JSON.stringify(catalog, null, 2));
+console.log(`catalog updated (${catalog.length} modules).`);
