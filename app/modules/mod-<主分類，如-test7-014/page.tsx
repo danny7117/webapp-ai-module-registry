ed91d82263738f4e598bb9db03d600a9b34c294b
danny7117 <210326565+danny7117@users.noreply.github.com>
2025-08-15 11:42:27 +0000

    chore: scaffold module from issue #14

---
+++ app/modules/mod-<主分類，如-test7-014/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+  return <main className="p-6">
+    <h1>test7</h1>
+    <p>Module ID: mod-<主分類，如-test7-014</p>
+  </main>;
+}
---
+++ modules/<主分類，如/mod-<主分類，如-test7-014/schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const mod_<主分類，如_test7_014_input = z.object({
+  // TODO: define from spec
+});
+
+export const mod_<主分類，如_test7_014_output = z.object({
+  // TODO: define from spec
+});
